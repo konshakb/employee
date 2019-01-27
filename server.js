66e8102ae797e14dd2c12e254fc09ec33053bb1a
Bryan Konshak <bkonshak@gmail.com>
2019-01-27 12:43:18 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require(`express`);
+
+const users = require(`./routes/api/users`);
+const profile = require(`./routes/api/profile`);
+const posts = require(`./routes/api/posts`);
+const app = express();
+
+app.get("/", (req, res) => res.send(`Hello `));
+
+//use routs
+app.use(`/api/users`, users);
+app.use(`/api/profile`, profile);
+app.use(`/api/posts`, posts);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
